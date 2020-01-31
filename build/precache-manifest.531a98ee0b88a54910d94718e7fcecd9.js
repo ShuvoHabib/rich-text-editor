c028413fefdc08e3ab52eb23adc6df62a41411aa
shuvo habib <shuvohabib@gmail.com>
2020-01-31 21:52:05 +0600

    add build

---
+++ build/precache-manifest.531a98ee0b88a54910d94718e7fcecd9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e790557c8d803b056f7a",
+    "url": "/static/js/main.e790557c.chunk.js"
+  },
+  {
+    "revision": "19c1ff90bc6c7b744c02",
+    "url": "/static/js/1.19c1ff90.chunk.js"
+  },
+  {
+    "revision": "e790557c8d803b056f7a",
+    "url": "/static/css/main.3ee89ad8.chunk.css"
+  },
+  {
+    "revision": "42136716b372ad91b59debbf2a9993fd",
+    "url": "/index.html"
+  }
+];
